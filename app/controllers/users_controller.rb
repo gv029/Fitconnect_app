1b4adec2108585a923c012711e4f29dd6da8ed4a
Ryan Bent <ryanbent01@gmail.com>
2015-09-11 22:11:41 -0500

    create new user option in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,27 @@ class UsersController < ApplicationController
   end
 
   def new
+    @users = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "User was created."
+      session[:user_id] = @user.id 
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :bio, :certifications)
+  end
+
 end
