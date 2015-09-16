30817b9552fd99cf34c8f9790ce816ce97a0ee8a
ryanb082 <ryanbent01@gmail.com>
2015-09-15 19:21:52 -0500

    Revert "Fix bio calculation error"

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,20 +5,18 @@ class User < ActiveRecord::Base
   D= 60
   F= 0
 
-  CLIENT_SCORE_WEIGHT = 2
-
   enum role: [ :trainer, :client, :admin ]
 
   validates :name, presence: true
 
   def update_score
-    #rG = file_uploads_score
+    rG = file_uploads_score
     cG = certifications_score
-    #bG = bio_score
-    #aG = time_score
+    bG = bio_score
+    aG = time_score
 
     puts "I'm here"
-    self.score = (((4) + (cG * CLIENT_SCORE_WEIGHT) + (5) + (3)) / 14).round(0)
+    self.score = (((rG * 4) + (cG * 2) + (bG * 5) + (aG * 3)) / 14).round(0)
 
     # ((rG * rW) + (cG * cW) + (bG * bW) + (aG * aW)) / (rW + cW + bW + aw)
     # ((70 * 4) + (CG * 2) + (bG * 5) + (aG * 3)) / (4 + 2 + 5 +3)
@@ -36,53 +34,53 @@ class User < ActiveRecord::Base
     end
   end
   
-  #def file_uploads_score
-#   number_of_files = self.file_uploads.count # nr_of_files = self.file_uploads.count#
+  def file_uploads_score
+   number_of_files = self.file_uploads.count # nr_of_files = self.file_uploads.count
 
-#    if number_of_files == 0 
-#      User::C 
-#    elsif (number_of_files == 1) || (number_of_files == 2) || (number_of_files == 3)
-#      # (number_of_files >= 1 &&  number_of_files <= 3)
-#      User::B
-#    elsif number_of_files >= 4
-#      User::A
-#    end
-#    # if nr_of_files == 0
-#    #   return 0
-#    # elsif nr_of_files == 1
-#    #   return 60
-#    # end 
-#  end
-
-
-  #def bio_score
-#    case self.bio.split(/\s+/).length
-#    when 0
-#      User::F 
-#    when (1..249)
-#        User::B
-#    else
-#        User::A
-#    end
-#  end
+    if number_of_files == 0 
+      User::C 
+    elsif (number_of_files == 1) || (number_of_files == 2) || (number_of_files == 3)
+      # (number_of_files >= 1 &&  number_of_files <= 3)
+      User::B
+    elsif number_of_files >= 4
+      User::A
+    end
+    # if nr_of_files == 0
+    #   return 0
+    # elsif nr_of_files == 1
+    #   return 60
+    # end 
+  end
 
 
-  #def time_score
-#    0
-#  end#
+  def bio_score
+    case bio.split(/\s+/).length
+    when 0
+      User::F 
+    when (1..249)
+      User::B
+    else
+      User::A
+    end
+  end
 
-#  def empty?
-#    bio.size == 0
-#  end#
 
-#  def half_full
-#    bio.size == 125 
-#  end#
+  def time_score
+    0
+  end
 
-#  def full
-#    bio.size == 250
-#  end
-#end
+  def empty?
+    bio.size == 0
+  end
+
+  def half_full
+    bio.size == 125 
+  end
+
+  def full
+    bio.size == 250
+  end
+end
 
 
 
@@ -93,4 +91,3 @@ class User < ActiveRecord::Base
 #
 # User has_may file_uplodas
 #
-end
