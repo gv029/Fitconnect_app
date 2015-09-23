a7a823f3c47e0cfbc2b512af891dde10d3f642b5
Ryan Bent <ryanbent01@gmail.com>
2015-09-22 20:46:47 -0500

    Add delete user function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id]).destroy
+    flash[:success] = 'User deleted'
+    redirect_to users_path
+  end
+
   def show
     @user = User.find(params[:id])
   end
